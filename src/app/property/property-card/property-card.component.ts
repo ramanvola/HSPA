2ac56dfc5690efef27041cc64da8862f9fbb618b
Raman Singh Panwar <ramanpanwar@eaton.com>
2022-05-15 10:20:17 +0530

    Property card css changes

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -6,15 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-card.component.css']
 })
 export class PropertyCardComponent implements OnInit {
-
+  @Input() property : any
   constructor() { }
 
-  Property:any = {
-    "Id":1,
-    "Name":"Birla house",
-    "Type":"House",
-    "Price": 12000
-  }
+
   ngOnInit() {
   }
 
