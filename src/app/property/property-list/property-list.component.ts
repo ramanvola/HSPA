ee71c71957f07561818db363d301ce9895c6c6a5
Raman Singh Panwar <ramanpanwar@eaton.com>
2022-05-18 20:19:28 +0530

    Add housing service to get the json data

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/Services/housing.service';
 
 @Component({
   selector: 'app-property-list',
@@ -7,59 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
-  properties: Array<any> = [
-    {
-      "Id": 1,
-      "Name": "Birla house",
-      "Type": "House",
-      "Price": 12000
-    },
-    {
-      "Id": 2,
-      "Name": "Erose Villa",
-      "Type": "House",
-      "Price": 15000
-    },
-    {
-      "Id": 3,
-      "Name": "Panwar Sadan",
-      "Type": "House",
-      "Price": 22000
-    },
-    {
-      "Id": 4,
-      "Name": "Kandari house",
-      "Type": "House",
-      "Price": 22000
-    },
-    {
-      "Id": 5,
-      "Name": "Albert Villa",
-      "Type": "House",
-      "Price": 21000
-    },
-    {
-      "Id": 6,
-      "Name": "Asha kutir",
-      "Type": "House",
-      "Price": 18000
-    },
-    {
-      "Id": 7,
-      "Name": "ABS house",
-      "Type": "House",
-      "Price": 52000
-    },
-    {
-      "Id": 8,
-      "Name": "ABC house",
-      "Type": "House",
-      "Price": 25000
-    }
-  ]
-  constructor() { }
+  properties: any ;
+  constructor(private housingService:HousingService) { }
 
   ngOnInit() {
+    this.housingService.getAllProperties().subscribe(
+      data => {
+        this.properties = data;
+        console.log(data);
+      }, error=>{
+        console.log(error);
+      }
+    )
   }
-
 }
